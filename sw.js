3ee718519081cb8094392d1e95b036b85a82f8df
Ueste-Telegram <vahitovilmir52@gmail.com>
2025-11-16 21:15:31 +0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,62 @@
+const CACHE_NAME = 'animanga-tracker-v1.2';
+const urlsToCache = [
+  '/animanga-tracker/',
+  '/animanga-tracker/index.html'
+];
+
+// Установка Service Worker
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Активация Service Worker
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Перехват запросов
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Возвращаем кэшированную версию или делаем запрос
+        if (response) {
+          return response;
+        }
+        
+        return fetch(event.request).then(function(response) {
+          // Проверяем валидность ответа
+          if(!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+
+          // Клонируем ответ
+          var responseToCache = response.clone();
+
+          caches.open(CACHE_NAME)
+            .then(function(cache) {
+              cache.put(event.request, responseToCache);
+            });
+
+          return response;
+        });
+      })
+    );
+});
